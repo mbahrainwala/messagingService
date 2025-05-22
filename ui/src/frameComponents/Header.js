524918cceb1ed1be810f7f43a83b29c43afe1068
Mustafa Behrainwala <mbahrainwala@hotmail.com>
2025-05-22 14:37:22 -0400

    Added header, footer and styles

--- ui/src/frameComponents/Header.js
+++ ui/src/frameComponents/Header.js
@@ -1,10 +1,12 @@
 import {useStyle} from "../utils/UseStyles";
+import {Link as RouterLink, Link} from 'react-router-dom'
 
 export const Header = props => {
     const classes = useStyle();
 
     return(
         <header>
+            <Link to="/">Home</Link>
             Header Goes Here
         </header>
     )
